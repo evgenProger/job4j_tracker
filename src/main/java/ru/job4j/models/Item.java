49783a9f7612b4ba2c013bebcdea8e596e6d6f29
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-12-10 15:38:01 +0300

    @Setter, @Getter and another [#504889]

--- src/main/java/ru/job4j/models/Item.java
+++ src/main/java/ru/job4j/models/Item.java
@@ -1,31 +1,48 @@
 package ru.job4j.models;
 
-import lombok.Data;
+import lombok.*;
 
 @Data
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
 
+    @Getter
+    @Setter
     private String id;
+
+    @Getter
+    @Setter
+    @NonNull
+    @EqualsAndHashCode.Include
     private String name;
+
+    @Getter
+    @Setter
+    @NonNull
     private String description;
+
+    @Getter
+    @Setter
+    @NonNull
     private long created;
 
-    public Item() {
+
+
+    public Item(@NonNull String name) {
+        this.name = name;
     }
 
-    public Item(String name, String description, long created) {
+    public Item(@NonNull String name, @NonNull String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Item(@NonNull String name, @NonNull String description, @NonNull long created) {
         this.name = name;
         this.description = description;
         this.created = created;
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(String name, String description) {
-        this.name = name;
-        this.description = description;
     }
 }
 

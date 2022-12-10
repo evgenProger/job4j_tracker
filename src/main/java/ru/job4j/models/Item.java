7339aed1aad329841a988fa0af3724b2040a2582
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-12-10 15:03:39 +0300

    Add lombok into Item model

--- src/main/java/ru/job4j/models/Item.java
+++ src/main/java/ru/job4j/models/Item.java
@@ -1,9 +1,8 @@
 package ru.job4j.models;
 
-import javax.persistence.*;
-import java.util.Objects;
+import lombok.Data;
 
-
+@Data
 public class Item {
 
     private String id;
@@ -13,7 +12,6 @@ public class Item {
 
     public Item() {
     }
-
 
     public Item(String name, String description, long created) {
         this.name = name;
@@ -25,76 +23,9 @@ public class Item {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id='"
-                + id
-                + '\''
-                + ", name='" + name
-                + '\''
-                + ", description='"
-                + description + '\''
-                + ", create="
-                + created + '}';
-    }
-
     public Item(String name, String description) {
         this.name = name;
         this.description = description;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String desc) {
-        this.description = desc;
-    }
-
-    public long getCreated() {
-        return this.created;
-    }
-
-    public void setCreated(long create) {
-        this.created = create;
-    }
-
-    public String getId() {
-        return this.id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return created == item.created
-                && Objects.equals(id, item.id)
-                && Objects.equals(name, item.name)
-                && Objects.equals(description, item.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, description, created);
     }
 }
 

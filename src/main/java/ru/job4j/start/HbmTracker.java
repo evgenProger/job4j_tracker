65811287ae79275c0241fd81f363507203db4fb2
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2023-01-15 17:48:19 +0300

    HbmTracker [#287705]. Рефактор.

--- src/main/java/ru/job4j/start/HbmTracker.java
+++ src/main/java/ru/job4j/start/HbmTracker.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
 import ru.job4j.models.Item;
 
 import java.util.List;
@@ -12,9 +13,11 @@ import java.util.List;
 public class HbmTracker implements Store, AutoCloseable {
 
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-            .configure().build();
+            .configure()
+            .build();
     private final SessionFactory sf = new MetadataSources(registry)
-            .buildMetadata().buildSessionFactory();
+            .buildMetadata()
+            .buildSessionFactory();
     private Session session;
 
     @Override
@@ -26,58 +29,79 @@ public class HbmTracker implements Store, AutoCloseable {
     public Item add(Item item) {
         session.beginTransaction();
         session.save(item);
-        session.getTransaction().commit();
+        session.getTransaction()
+               .commit();
         session.clear();
         return item;
     }
 
     @Override
     public boolean replace(String id, Item item) {
-        session.beginTransaction();
-        session.update(id, item);
-        session.getTransaction().commit();
+        boolean result = false;
+        try {
+            session.beginTransaction();
+            Query query = session.createQuery("update Item set name = :fName where id = :fId");
+            query.setParameter("fName", item.getName());
+            query.setParameter("fId", id);
+            result = query.executeUpdate() == 1;
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
         session.close();
-        return true;
-    }
-
-    @Override
-    public boolean delete(String id) {
-        session.beginTransaction();
-        session.delete(findById(id));
-        session.getTransaction().commit();
-        session.close();
-        return true;
-    }
-
-    @Override
-    public List<Item> findAll() {
-        session.beginTransaction();
-        List items = session.createQuery("from ru.job4j.models.Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
-    }
-
-    @Override
-    public List<Item> findByName(String key) {
-        session.beginTransaction();
-        List items = session.createQuery("from Item as i where i.name = 'key'", Item.class).list();
-        session.getTransaction().commit();
-        session.close();
-        return items;
-    }
-
-    @Override
-    public Item findById(String id) {
-       session.beginTransaction();
-       Item result = session.get(Item.class, id);
-       session.getTransaction().commit();
-       session.close();
         return result;
     }
 
     @Override
-    public void close() throws Exception {
-        StandardServiceRegistryBuilder.destroy(registry);
+    public boolean delete(String id) {
+        boolean result = false;
+        try {
+            session.beginTransaction();
+            Query query = session.createQuery("delete Item where id = :fId");
+            query.setParameter("fId", id);
+            result = query.executeUpdate() == 1;
+            session.getTransaction()
+                   .commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+            session.close();
+            return result;
+        }
+
+        @Override
+        public List<Item> findAll () {
+            session.beginTransaction();
+            List items = session.createQuery("from Item")
+                                .list();
+            session.getTransaction()
+                   .commit();
+            session.close();
+            return items;
+        }
+
+        @Override
+        public List<Item> findByName (String key) {
+            session.beginTransaction();
+            Query<Item> query = session.createQuery("from Item as i where i.name = :fName", Item.class);
+            query.setParameter("fName", key);
+            query.executeUpdate();
+            session.close();
+            return query.getResultList();
+        }
+
+        @Override
+        public Item findById (String id){
+            session.beginTransaction();
+            Item result = session.get(Item.class, id);
+            session.getTransaction()
+                   .commit();
+            session.close();
+            return result;
+        }
+
+        @Override
+        public void close () throws Exception {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
     }
-}
